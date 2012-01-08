c7147fac130e7b967ee712ed5c2817fe39565f1a
juanantonioruz <juanantonioruz@gmail.com>
2012-01-08 18:11:49 +0100

    hacemos por encargo ok

---
+++ SERVER6/category-hacemos-por-encargo.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage Default_Theme
+ */
+$valor_header=array("HACEMOS POR ENCARGO", "WE DO IT");
+include("header.php");
+//$ejemplo=preparaContenido(get_the_content());
+//$col1_up=encuentra("COL1_UP",$ejemplo);
+//echo $col1_up[0];
+?>
+<?php 
+query_posts(array('showposts'=>4, 'category_name' => 'Hacemos por Encargo')); ?>
+<div id="encargo_container">
+   	<?php if (have_posts()) : ?>
+		<?php while (have_posts()) : the_post(); 
+		$ejemplo=preparaContenido(get_the_content());
+		$columnas=encuentra("COLUMN",$ejemplo);
+			$count++;
+		?>
+			<div class="encargo_col_izq_bis">
+
+<?=$columnas[0]?>
+
+	</div>
+	<div class="encargo_col_dcha_bis">
+<?=$columnas[1]?>
+	</div>
+	
+<hr>
+				<?php endwhile; ?>
+	<?php endif; ?>
+</div>
+	
+
+
+
+
+
+
+<?php 
+include("footer.php");
+ ?>
---
+++ SERVER6/page-contactanos.php
@@ -0,0 +1,39 @@
+<?php
+/*
+Template Name: Cover Page
+*/
+$valor_header=array("CONTACTANOS", "CONTACT");
+include("header.php");
+?>
+<div id="noticias_container">
+
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+	<div class="noticia">
+		<div class="visual_noticia">
+			<img src="http://rinocerontefilms.com/wp-content/uploads/template/GIF_RINO_baja_final.gif">
+		</div>
+		<div class="cabecera_noticia">
+			<?php the_title(); ?>
+		</div>
+		<div class="contenido_noticia" style="text-align:center;">
+			<?php the_content(); ?>
+
+<img id="Image-Maps_2201201072000463" src="http://rinocerontefilms.com/wp-content/uploads/template/siguenos.png" usemap="#Image-Maps_2201201072000463" border="0" width="175" height="31" alt=""  style="margin-top:10px;"/>
+<map id="_Image-Maps_2201201072000463" name="Image-Maps_2201201072000463">
+<area shape="rect" coords="78,0,104,26" href="http://www.twitter.com/" alt="twitter" title="twitter"    />
+<area shape="rect" coords="111,0,137,26" href="https://www.facebook.com/pages/Rinoceronte-Films/167780846597141?sk=wall" alt="facebook" title="facebook"    />
+<area shape="rect" coords="149,0,170,26" href="http://vimeo.com/rinocerontefilms" alt="vimeo" title="vimeo"    />
+<area shape="rect" coords="173,29,175,31" href="http://www.image-maps.com/index.php?aff=mapped_users_2201201072000463" alt="Image Map" title="Image Map" />
+</map>
+
+		</div>
+	</div>
+			<?php endwhile; endif; ?>
+			
+	
+	
+</div>
+
+	
+		
+<?php include("footer.php"); ?>
