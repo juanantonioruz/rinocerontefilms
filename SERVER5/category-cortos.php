6b01c124dd71eafe4b5574994c62aef2ee11b229
juanantonioruz <juanantonioruz@gmail.com>
2012-01-08 14:31:42 +0100

    detalle films ok

---
+++ SERVER5/category-cortos.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage Default_Theme
+ */
+$valor_header=array("CORTOS", "SHORTS");
+include("header.php");
+$count = 0;
+
+?>
+<div id="quienes_somos_container" class="showgrid">
+
+<?php 
+query_posts(array('showposts'=>4, 'category_name' => 'cortos')); ?>
+   	<?php if (have_posts()) : ?>
+		<?php while (have_posts()) : the_post(); 
+			$count++;
+		?>
+			<div class="film" style="<?=($count==1) ? 'float: left;' :'' ?>">
+				<div class="visual_film">
+<?php
+if ( has_post_thumbnail() )
+		the_post_thumbnail( 'thumbnail' );
+?>
+				</div>
+				<div class="cabecera_film">
+				<?php the_title(); ?>
+				</div>
+				<div class="contenido_film">
+<?php the_content(__('Read the rest of this entry &raquo;', 'kubrick')); ?>
+				</div>
+				<div style="text-align:right;">
+				<!--div class="img_mas" ><a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php printf(__('Permanent Link to %s', 'kubrick'), the_title_attribute('echo=0')); ?>"><img src="imgs/mas.png"></a></div -->
+				<!--div class="saber_mas_film"><a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php printf(__('Permanent Link to %s', 'kubrick'), the_title_attribute('echo=0')); ?>">SABER MAS</a></div -->
+				</div>
+			</div>
+		<?php endwhile; ?>
+	<?php endif; ?>
+</div>
+	
+
+
+                	
+
+
+
+
+
+
+
+
+<?php 
+include("footer.php");
+?>
---
+++ SERVER5/pelicula_detalle.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage Default_Theme
+ */
+ /*
+Template Name: Detalle_Pelicula
+*/
+$valor_header=array("CORTOS", "SHORTS");
+include("header.php");
+
+?>
+
+<div id="encargo_container">
+	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+	<?php
+		$ejemplo=preparaContenido(get_the_content());
+		$cols=encuentra("COLUMN",$ejemplo);
+		?>
+	<div id="encargo_col_izq">
+	<?=$cols[0]?>
+
+	</div>
+	<div id="encargo_col_dcha">
+		<?=$cols[1]?>
+</div>
+		<?php endwhile; endif; ?>
+</div>
+
+<?php 
+include("footer.php");
+ ?>
